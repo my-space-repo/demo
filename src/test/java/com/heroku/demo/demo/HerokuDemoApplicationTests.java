78448f50e8989c79ef5fc44cd3bc148c22c9e6f6
subr1990 <subramaniank@live.com>
2020-09-03 22:11:04 +1000

    Initial commit

---
+++ src/test/java/com/heroku/demo/demo/HerokuDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroku.demo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
